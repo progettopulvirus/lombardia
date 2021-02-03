f161ce2818d5ec207b672a09085f43d97ca44427
guidofioravanti <guidofioravanti@yahoo.it>
2021-02-03 21:50:47 +0100

    Integratidati mancanti del PM10, caricati dati del benzene

--- R/pm10.R
+++ R/pm10.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di pm10 per 18 stazioni della regione LOMBARDIA
+#' Tibble con i dati di pm10 per 51 stazioni della regione LOMBARDIA
 #'
-#' @format Un tibble con 8 colonne e 50958 osservazioni
+#' @format Un tibble con 8 colonne e 143586 osservazioni
 #'
 #' @usage
 #' pm10
